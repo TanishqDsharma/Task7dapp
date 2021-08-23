875f138fe57850869ff9ebc148fb3ddd820d977d
TanishqDsharma <tanishqsharma612@gmail.com>
2021-08-23 08:12:41 -0400

    update

---
+++ src/lib/contracts/SimpleStorageWrapper.ts
@@ -0,0 +1,52 @@
+import Web3 from 'web3';
+import * as SimpleStorageJSON from '../../../build/contracts/SimpleStorage.json';
+import { SimpleStorage } from '../../types/SimpleStorage';
+
+export class SimpleStorageWrapper {
+    web3: Web3;
+
+    contract: SimpleStorage;
+
+    address: string;
+
+    constructor(web3: Web3) {
+        this.web3 = web3;
+        this.contract = new web3.eth.Contract(SimpleStorageJSON.abi as any) as any;
+    }
+
+    get isDeployed() {
+        return Boolean(this.address);
+    }
+
+    async getStoredValue(fromAddress: string) {
+        const data = await this.contract.methods.get().call({ from: fromAddress });
+
+        return parseInt(data, 10);
+    }
+
+    async setStoredValue(value: number, fromAddress: string) {
+        const tx = await this.contract.methods.set(value).send({
+            from: fromAddress
+        });
+
+        return tx;
+    }
+
+    async deploy(fromAddress: string) {
+        const contract = await this.contract
+            .deploy({
+                data: SimpleStorageJSON.bytecode,
+                arguments: []
+            })
+            .send({
+                from: fromAddress
+            });
+
+        this.useDeployed(contract.options.address);
+    }
+
+    useDeployed(contractAddress: string) {
+        this.address = contractAddress;
+        this.contract.options.address = contractAddress;
+    }
+}
---
+++ src/ui/app.tsx
@@ -0,0 +1,298 @@
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-use-before-define */
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { ToastContainer, toast } from 'react-toastify';
+import './app.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import { PolyjuiceHttpProvider } from '@polyjuice-provider/web3';
+import { AddressTranslator } from 'nervos-godwoken-integration';
+import { CONFIG } from '../config';
+// import { SimpleStorageWrapper } from '../lib/contracts/SimpleStorageWrapper';
+import { TodoListWrapper } from '../lib/contracts/TodoListWrapper';
+
+async function createWeb3() {
+    // Modern dapp browsers...
+    if ((window as any).ethereum) {
+        // const web3 = new Web3((window as any).ethereum);
+        const godwokenRpcUrl = CONFIG.WEB3_PROVIDER_URL;
+        const providerConfig = {
+            rollupTypeHash: CONFIG.ROLLUP_TYPE_HASH,
+            ethAccountLockCodeHash: CONFIG.ETH_ACCOUNT_LOCK_CODE_HASH,
+            web3Url: godwokenRpcUrl
+        };
+        const provider = new PolyjuiceHttpProvider(godwokenRpcUrl, providerConfig);
+        const web3 = new Web3(provider || Web3.givenProvider);
+        try {
+            // Request account access if needed
+            await (window as any).ethereum.enable();
+        } catch (error) {
+            // User denied account access...
+        }
+
+        return web3;
+    }
+
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    return null;
+}
+
+export function App() {
+    const [web3, setWeb3] = useState<Web3>(null);
+    const [contract, setContract] = useState<TodoListWrapper>();
+    const [accounts, setAccounts] = useState<string[]>();
+    const [balance, setBalance] = useState<bigint>();
+    const [existingContractIdInputValue, setExistingContractIdInputValue] = useState<string>();
+    const [taskDesc, setTaskDesc] = useState<string | undefined>();
+    const [taskId, setTaskId] = useState<number | undefined>();
+    const [taskList, setTaskList] = useState<any | undefined>();
+    const [taskCount, setTaskCount] = useState<number | undefined>();
+    const [transactionInProgress, setTransactionInProgress] = useState(false);
+    const [polyjuiceAddress, setPolyjuiceAddress] = useState<string | undefined>();
+    const toastId = React.useRef(null);
+    const [newStoredNumberInputValue, setNewStoredNumberInputValue] = useState<
+        number | undefined
+        >();
+    useEffect(() => {
+        if (accounts?.[0]) {
+            const addressTranslator = new AddressTranslator();
+            setPolyjuiceAddress(addressTranslator.ethAddressToGodwokenShortAddress(accounts?.[0]));
+        } else {
+            setPolyjuiceAddress(undefined);
+        }
+    }, [accounts?.[0]]);
+    useEffect(() => {
+        if (transactionInProgress && !toastId.current) {
+            toastId.current = toast.info(
+                'Transaction in progress. Confirm MetaMask signing dialog and please wait...',
+                {
+                    position: 'top-right',
+                    autoClose: false,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    closeButton: false
+                }
+            );
+        } else if (!transactionInProgress && toastId.current) {
+            toast.dismiss(toastId.current);
+            toastId.current = null;
+        }
+    }, [transactionInProgress, toastId.current]);
+
+    const account = accounts?.[0];
+    
+    const SimpleList = ({list} : {list:any}) => (
+        <ul>
+            {list.map((item: any) => (
+                <li key={item.id}>
+                    <label>Task ID</label><div>{item.id}</div>
+                    <label>Task Title</label>
+                    <div>{item.title}</div>
+                    <label>Task Completed</label>
+                    <div>{item.completed.toString()}</div>
+                </li>
+            ))}
+        </ul>
+    );
+    async function deployContract() {
+        const _contract = new TodoListWrapper(web3);
+
+        try {
+            setTransactionInProgress(true);
+
+            await _contract.deploy(account);
+
+            setExistingContractAddress(_contract.address);
+            toast(
+                'Successfully deployed a smart-contract. You can now proceed to get or set the value in a smart contract.',
+                { type: 'success' }
+            );
+        } catch (error) {
+            console.error(error);
+            toast.error(
+                'There was an error sending your transaction. Please check developer console.'
+            );
+        } finally {
+            setTransactionInProgress(false);
+        }
+    }
+    async function createTask(){
+        try{
+            setTransactionInProgress(true);
+            await contract.createTask(taskDesc, account);
+            toast(
+                'Successfully added to task list. You can refresh the read value now manually.',
+                { type: 'success' }
+            );
+        } catch (error) {
+            console.error(error);
+            toast.error(
+                'There was an error sending your transaction. Please check developer console.'
+            );
+        } finally {
+            setTransactionInProgress(false);
+        }
+
+    }
+    async function renderTaskCount() {
+        const task_count = await contract.getTaskCount(account);
+        toast('Successfully read taskCount.', { type: 'success' });
+        setTaskCount(task_count);
+    }
+    async function renderTask(){
+        const task_list= await contract.getTaskList(taskCount,account)
+        console.log(task_list)
+        setTaskList(task_list);
+    }
+    async function toggleCompleted(){
+        try{
+            setTransactionInProgress(true);
+            await contract.toggleTaskCompleted(taskId, account);
+            console.log('Task Completed', taskId);
+            toast(
+                'Successfully toggled task to completed. You can refresh the read value now manually.',
+                { type: 'success' }
+            );
+        } catch (error) {
+            console.error(error);
+            toast.error(
+                'There was an error sending your transaction. Please check developer console.'
+            );
+        } finally {
+            setTransactionInProgress(false);
+        }
+    }
+    // async function getStoredValue() {
+    //     const value = await contract.getStoredValue(account);
+    //     toast('Successfully read latest stored value.', { type: 'success' });
+    //
+    //     setStoredValue(value);
+    // }
+
+    async function setExistingContractAddress(contractAddress: string) {
+        const _contract = new TodoListWrapper(web3);
+        _contract.useDeployed(contractAddress.trim());
+
+        setContract(_contract);
+        // setStoredValue(undefined);
+    }
+
+    // async function setNewStoredValue() {
+    //     try {
+    //         setTransactionInProgress(true);
+    //         await contract.setStoredValue(newStoredNumberInputValue, account);
+    //         toast(
+    //             'Successfully set latest stored value. You can refresh the read value now manually.',
+    //             { type: 'success' }
+    //         );
+    //     } catch (error) {
+    //         console.error(error);
+    //         toast.error(
+    //             'There was an error sending your transaction. Please check developer console.'
+    //         );
+    //     } finally {
+    //         setTransactionInProgress(false);
+    //     }
+    // }
+
+    useEffect(() => {
+        if (web3) {
+            return;
+        }
+
+        (async () => {
+            const _web3 = await createWeb3();
+            setWeb3(_web3);
+
+            const _accounts = [(window as any).ethereum.selectedAddress];
+            setAccounts(_accounts);
+            console.log({ _accounts });
+
+            if (_accounts && _accounts[0]) {
+                const _l2Balance = BigInt(await _web3.eth.getBalance(_accounts[0]));
+                setBalance(_l2Balance);
+            }
+        })();
+    });
+
+    const LoadingIndicator = () => <span className="rotating-icon">⚙️</span>;
+
+    return (
+        <div>
+            Your ETH address: <b>{accounts?.[0]}</b>
+            <br />
+            <br />
+            Your Polyjuice address: <b>{polyjuiceAddress}</b>
+            <br />
+            <br />
+            Balance: <b>{balance ? (balance / 10n ** 8n).toString() : <LoadingIndicator />} ETH</b>
+            <br />
+            <br />
+            Deployed contract address: <b>{contract?.address || '-'}</b> <br />
+            <br />
+            <hr />
+            <p>
+                The button below will deploy a SimpleStorage smart contract where you can store a
+                number value. By default the initial stored value is equal to 123 (you can change
+                that in the Solidity smart contract). After the contract is deployed you can either
+                read stored value from smart contract or set a new one. You can do that using the
+                interface below.
+            </p>
+            <button onClick={deployContract} disabled={!balance}>
+                Deploy contract
+            </button>
+            &nbsp;or&nbsp;
+            <input
+                placeholder="Existing contract id"
+                onChange={e => setExistingContractIdInputValue(e.target.value)}
+            />
+            <button
+                disabled={!existingContractIdInputValue || !balance}
+                onClick={() => setExistingContractAddress(existingContractIdInputValue)}
+            >
+                Use existing contract
+            </button>
+            <br />
+            <br />
+            <button onClick={renderTaskCount} disabled={!contract}>
+                Get Task Count
+            </button>
+            {taskCount ? <>&nbsp;&nbsp;Task Count: {taskCount.toString()}</> : null}
+            <br />
+            <br />
+            <button onClick={renderTask} disabled={!contract}>
+                Get Task List
+            </button>
+            {taskList ? <SimpleList list={taskList} /> : null}
+            <br />
+            <br />
+            <input
+                placeholder="Enter Task Id"
+                type="number"
+                onChange={e => setTaskId(parseInt(e.target.value,10))}
+            />
+            <button onClick={toggleCompleted} disabled={!contract}>
+                Complete Task
+            </button>
+            <input
+                placeholder="Enter Task Title"
+                type="text"
+                onChange={e => setTaskDesc(e.target.value)}
+            />
+            <button onClick={createTask} disabled={!contract}>
+                Submit new task
+            </button>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <hr />
+            <ToastContainer />
+        </div>
+    );
+}
